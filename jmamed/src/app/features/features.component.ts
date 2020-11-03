995cc3c88c0f49ba1b277529c5c085b8be7ebe77
Jason Mamed <mamed.jason@gmail.com>
2020-11-03 12:31:59 -0600

    complete geolocation feature, audit project

--- jmamed/src/app/features/features.component.ts
+++ jmamed/src/app/features/features.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GeolocationService } from './../services/geolocation.service';
 
 @Component({
   selector: 'features',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturesComponent implements OnInit {
 
-  constructor() { }
+  latitude: any;
+  longitude: any;
+  accuracy: any;
+  altitude: any;
+  altitudeAccuracy: any;
+  heading: any;
+  speed: any;
+  showArea: boolean = false;
+  errorArea: boolean = false;
+  useragent: any;
+
+  constructor(
+    private geoService: GeolocationService,
+  ) { }
 
   ngOnInit() {
+    this.geoService.getGeolocation();//trigger popup on load
   }
 
+  getGeoData(){
+    this.latitude = this.geoService.latitude;
+    this.longitude = this.geoService.longitude;
+    this.accuracy = this.geoService.accuracy;
+    this.altitude = this.geoService.altitude;
+    this.altitudeAccuracy = this.geoService.altitudeAccuracy;
+    this.heading = this.geoService.heading;
+    this.speed = this.geoService.speed;
+  }
+
+  showgeolocation() {//toggle view
+    this.getGeoData();
+    if (this.geoService.geoError) {
+      this.errorArea = true;
+      if (!this.geoService.geoConsent) {
+        this.showArea = false;
+      }
+    }
+    else {
+      if (this.geoService.geoConsent) {
+        this.showArea = true;
+      }
+    }
+  }
 }
