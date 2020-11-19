ce8acfe7a786fd1f1f575b2c0b736a5b68a05779
Jason Mamed <mamed.jason@gmail.com>
2020-11-18 22:14:50 -0600

    created a progress ring with count up and reset button

--- jmamed/src/app/features/features.component.ts
+++ jmamed/src/app/features/features.component.ts
@@ -1,4 +1,7 @@
+// import { analyzeAndValidateNgModules } from '@angular/compiler';
+// import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+// import { Button } from 'protractor';
 import { GeolocationService } from './../services/geolocation.service';
 
 @Component({
@@ -8,6 +11,7 @@ import { GeolocationService } from './../services/geolocation.service';
 })
 export class FeaturesComponent implements OnInit {
 
+  //geo
   latitude: any;
   longitude: any;
   accuracy: any;
@@ -19,12 +23,27 @@ export class FeaturesComponent implements OnInit {
   errorArea: boolean = false;
   useragent: any;
 
+  //progress ring
+  circle : any;
+  radius : any;
+  circumference : number;
+  max: number;
+  current: number;
+  newValue: string;
+  next: number;
+  circleDone: boolean = false;
+
+
   constructor(
     private geoService: GeolocationService,
   ) { }
 
   ngOnInit() {
+    //geolocation
     this.geoService.getGeolocation();//trigger popup on load
+
+    //progress ring
+    this.progressRing();//initialize
   }
 
   getGeoData(){
@@ -51,4 +70,55 @@ export class FeaturesComponent implements OnInit {
       }
     }
   }
+
+  progressRing(){//run first, define circle
+    let circle = document.querySelector('circle');
+    let radius = circle.r.baseVal.value;
+    this.circumference = radius * 2 * Math.PI;
+    circle.style.strokeDasharray = `${this.circumference} ${this.circumference}`;
+    circle.style.strokeDashoffset = `${this.circumference}`;
+    this.max = 10; //ma
+    this.current = 0;
+    // console.log("Max " + this.max + "," + "Current " + this.current)
+    this.setProgress(this.current);
+  }
+  onClick(){
+    // if (input.value < 101 && input.value > -1) {
+    //   this.setProgress(input.value);
+    // }
+
+    if (this.max >= this.current){
+      this.current = this.current + 1;
+      this.next = this.current / this.max;
+      this.setProgress(this.next);
+    }
+  }
+  setProgress(percent: number){
+    if(this.max >= this.current){//this.max remains the constant from page load
+      // console.log("Percent " + percent);
+      // console.log("Circumference " + this.circumference);
+      const offset = this.circumference - (this.circumference * percent);
+      this.newValue = offset.toString();
+      // console.log(offset);
+      let circle = document.querySelector('circle');
+      circle.style.strokeDashoffset = this.newValue;
+    }
+    else{
+      console.log("Circle is complete")
+      this.circleDone = true;
+      this.disabledBtn();
+    }
+  }
+  disabledBtn(){
+    document.getElementById('progressButton').disabled = true;
+    // alert("Circle is complete"); //annoying alert message
+  }
+
+  onReset(){
+    this.current = 0;
+    this.circleDone = false;
+    document.getElementById('progressButton').disabled = false;
+    this.setProgress(0);
+  }
+
 }
